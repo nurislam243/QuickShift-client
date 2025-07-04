9697ceb78e074e4a298a3fc5ed5d6f3fb9c9cf08
nurislam243 <mdnurislam15177@gmail.com>
2025-07-04 17:36:23 +0600

    added firebase config and auth context

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -6,18 +6,18 @@ import QuickShiftLogo from '../pages/shared/QuickShiftLogo/QuickShiftLogo';
 const AuthLayout = () => {
     return (
         <div>
-            <div className="p-12 bg-base-200">
+            <div className="p-12 bg-base-200 max-w-[1536px] mx-auto min-h-screen">
                 <div className="">
                     <QuickShiftLogo></QuickShiftLogo>
                 </div>
-                <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="flex justify-center gap-[110px] flex-col lg:flex-row-reverse mt-[11]">
                     <div className="flex-1">
                         <img
                         src={authImage}
                         className=""
                         />
                     </div>
-                    <div className='flex-1'>
+                    <div className='flex-1 mt-14'>
                         <Outlet></Outlet>
                     </div>
                 </div>
--- src/pages/Authentication/Register/Register.jsx
+++ src/pages/Authentication/Register/Register.jsx
@@ -1,9 +1,52 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import useAuth from '../../../hooks/useAuth';
+import { Link } from 'react-router';
 
 const Register = () => {
+    const { createUser } = useAuth();
+     const { handleSubmit, register, formState: { errors } } = useForm();
+     
+
+     const onSubmit = data => {
+        console.log(data);
+        console.log(createUser);
+        createUser(data.email, data.password)
+        .then( result =>{
+            console.log(result.user)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+     }
+
     return (
         <div>
-            
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <fieldset className="fieldset">
+                    {/* email field */}
+                    <label className="label">Email</label>
+                    <input 
+                        type="email" 
+                        {...register('email')}
+                        className="input w-full" 
+                        placeholder="Email" 
+                    />
+
+
+                    {/* password field */}
+                    <label className="label">Password</label>
+                    <input 
+                        type="password" 
+                        {...register('password')}
+                        className="input w-full" 
+                        placeholder="Password" 
+                    />
+                    
+                    <button className="btn btn-primary text-black mt-4">Register</button>
+                </fieldset>
+                <p><small>Already have an account? <Link to={'/login'} className='btn btn-link'>Login</Link></small></p>
+            </form>
         </div>
     );
 };
