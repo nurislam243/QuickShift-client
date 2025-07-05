bf3fe5202af8ce8b0a9673b3d79d51c7839cd9e9
nurislam243 <mdnurislam15177@gmail.com>
2025-07-05 07:41:30 +0600

    added social login

--- src/pages/Authentication/Login/Login.jsx
+++ src/pages/Authentication/Login/Login.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router';
+import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Login = () => {
     const {register, handleSubmit, formState: { errors }} = useForm();
@@ -12,11 +14,15 @@ const Login = () => {
             <form onSubmit={handleSubmit(onSubmit)}>
                 <fieldset className="fieldset">
                     <label className="label">Email</label>
-                    <input type="email" className="input" placeholder="Email"
-                    {...register('email')} />
+                    <input type="email" className="input w-full" placeholder="Email"
+                    {...register('email', {required: true})} />
+                    {
+                        errors.email?.type === 'required' && <p className='text-error'>Email is required</p>
+                    }
+
                     <label className="label">Password</label>
                     <input 
-                    type="password" className="input" placeholder="Password" 
+                    type="password" className="input w-full" placeholder="Password" 
                     {...register('password', {required: true, minLength: 6})}/>
                     {
                         errors.password?.type === 'required' && <p className='text-error'>Password is required</p>
@@ -27,9 +33,14 @@ const Login = () => {
 
                     <div><a className="link link-hover">Forgot password?</a></div>
 
-                    <button className="btn btn-neutral mt-4">Login</button>
+                    <button className="btn btn-primary text-black mt-4">Login</button>
+                    <p><small>New to this website <Link to={'/register'}  className='btn btn-link'>Register</Link> </small></p>
                 </fieldset>
             </form>
+            <div className="flex justify-center mb-2.5">
+                <p>OR</p>
+            </div>
+            <SocialLogin></SocialLogin>
         </div>
     );
 };
